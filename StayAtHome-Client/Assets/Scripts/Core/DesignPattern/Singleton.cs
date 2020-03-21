51847545b723801fdd4177be708e9a0723005c6b
Michael Fettin <38879134+MichaelFettin@users.noreply.github.com>
2020-03-21 09:53:53 +0100

    Add Singleton pattern; Create GameManager

---
+++ StayAtHome-Client/Assets/Scripts/Core/DesignPattern/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Core.DesignPattern
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        public static T Instance { get; private set; }
+
+        private protected virtual void Awake()
+        {
+            if (Instance != null)
+                Destroy(gameObject);
+            else
+                Instance = (T)this;
+        }
+
+        private protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+    }
+}
