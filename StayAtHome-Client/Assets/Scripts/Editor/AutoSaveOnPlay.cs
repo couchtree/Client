1d241e780e143a0b33ab394ad3d8af776f00a1ed
Michael Fettin <mfettin@hotmail.de>
2020-03-21 01:06:59 +0100

    Add AutoSaveOnPlay script

---
+++ StayAtHome-Client/Assets/Scripts/Editor/AutoSaveOnPlay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public class AutoSaveOnPlay
+    {
+        static AutoSaveOnPlay()
+        {
+            EditorApplication.playModeStateChanged += SaveOnPlay;
+        }
+
+        private static void SaveOnPlay(PlayModeStateChange state)
+        {
+            if (state != PlayModeStateChange.ExitingEditMode)
+                return;
+            
+            Debug.LogWarning("Auto-saving...");
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
