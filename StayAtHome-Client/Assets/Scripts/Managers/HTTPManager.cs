7b6630c5529c38a4a21161f80ae254dbc82828ad
Michael Fettin <mfettin@hotmail.de>
2020-03-21 17:01:47 +0100

    Added POST request method

--- StayAtHome-Client/Assets/Scripts/Managers/HTTPManager.cs
+++ StayAtHome-Client/Assets/Scripts/Managers/HTTPManager.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class HTTPManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Test url
+        StartCoroutine(PostRequest("https://prooxey.de/endpoint.php", "{\"test\":\"djhgfusdhufg\"}"));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PostRequest(string url, string json)
     {
+        var uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
         
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
     }
 }
