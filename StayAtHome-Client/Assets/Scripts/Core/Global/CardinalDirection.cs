6f14e8f036676d602b0f946e2183f12a8095aaf4
Michael Fettin <mfettin@hotmail.de>
2020-03-21 12:20:32 +0100

    Add TMP; Add simple map; Add MapManager

---
+++ StayAtHome-Client/Assets/Scripts/Core/Global/CardinalDirection.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Global
+{
+    public struct CardinalDirection
+    { 
+        public const float NorthWest = 315f;
+        public const float North = 0f;
+        public const float NorthEast = 45f;
+        public const float East = 90f;
+        public const float SouthEast = 135f;
+        public const float South = 180f;
+        public const float SouthWest = 225f;
+        public const float West = 270f;
+    }
+}
---
+++ StayAtHome-Client/Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Core.DesignPattern;
+using Core.Global;
+using Core.Map;
+using Debug = UnityEngine.Debug;
+
+namespace Managers
+{
+    public class MapManager : Singleton<MapManager>
+    {
+        public GameObject playerPrefab;
+        public GameObject nearPlayerGroup;
+
+        GameObject player;
+        List<GameObject> nearPlayers = new List<GameObject>();
+
+        private void Start()
+        {
+            player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
+
+        private void Update()
+        {
+            TestFunction();
+        }
+
+        private void TestFunction()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+                AddPlayer("Test", CardinalDirection.NorthEast, 4f);
+        }
+
+        public void AddPlayer(String name, float direction, float distance)
+        {
+            var position = new Vector3(distance, 0, 0);
+            var rotation = Quaternion.Euler(0, 0, direction);
+            var newPlayer = Instantiate(playerPrefab, transform.position, rotation);
+
+            var player = newPlayer.GetComponent<Player>();
+
+            player.name = name;
+            player.distance = distance;
+            
+            newPlayer.name = "Player " + name;
+            newPlayer.transform.parent = nearPlayerGroup.transform;
+
+            newPlayer.transform.Translate(position);
+            newPlayer.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            
+            Debug.Log("Near player : " + name + " - Distance: " + distance);
+        }
+    }
+}
