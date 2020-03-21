59a5ddbed89b1cc5804333965e62136adda8fdce
CaluvH <opti01@gmx.net>
2020-03-21 17:56:47 +0100

    Added delegate to handle server response.

--- StayAtHome-Client/Assets/Scripts/Managers/HTTPManager.cs
+++ StayAtHome-Client/Assets/Scripts/Managers/HTTPManager.cs
@@ -8,31 +8,37 @@ namespace Managers
 {
     public class HTTPManager : Singleton<HTTPManager>
     {
-        public void SendRequest(string json)
+        public delegate void ServerResponse(string response);
+
+
+        public void SendRequest(string requestJson, ServerResponse response)
         {
             var url = "https://prooxey.de/endpoint.php";
-            
-            StartCoroutine(PostRequest(url, json));
+
+            StartCoroutine(PostRequest(url, requestJson, response));
         }
 
-        IEnumerator PostRequest(string url, string json)
+        IEnumerator PostRequest(string url, string json, ServerResponse response)
         {
             var uwr = new UnityWebRequest(url, "POST");
             byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
             uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
             uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
             uwr.SetRequestHeader("Content-Type", "application/json");
-            
+
             yield return uwr.SendWebRequest();
 
             if (uwr.isNetworkError)
             {
+                response(uwr.error);
                 Debug.Log("Error While Sending: " + uwr.error);
             }
             else
             {
+                response(uwr.downloadHandler.text);
                 Debug.Log("Received: " + uwr.downloadHandler.text);
             }
         }
+
     }
 }
