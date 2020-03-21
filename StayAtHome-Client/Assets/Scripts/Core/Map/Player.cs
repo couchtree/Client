7878d0fa04fb52cb262c504a9402933e47f4bee6
Mark <sprotte@eventit.ag>
2020-03-21 15:42:09 +0100

    add changeScore for Player

--- StayAtHome-Client/Assets/Scripts/Core/Map/Player.cs
+++ StayAtHome-Client/Assets/Scripts/Core/Map/Player.cs
@@ -9,19 +9,25 @@ namespace Core.Map
     public class Player : MonoBehaviour, ISaveable
     {
         public const String filename = "player";
-        
+
         [HideInInspector]
         public string name;
-        
+
         [HideInInspector]
         public float distance;
 
         public string Name { get; set; }
         public long lat { get; set; }
         public long lon { get; set; }
+        public int score { get; set; }
 
         private void Update()
         {
+        }
+
+        public void ChangeScore(int amount)
+        {
+            score += amount;
         }
 
         public IDataForSerialization GenerateSaveableData()
