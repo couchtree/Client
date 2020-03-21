78ca93a403ca715f6efcf8be0cd638822f39f882
Alex Osswald <pallandotheblue@hotmail.de>
2020-03-21 20:20:21 +0100

    Added home screen

---
+++ StayAtHome-Client/Assets/Scripts/UIHomeScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIHomeScreen : MonoBehaviour
+{
+
+    public GameObject overlay;
+
+    public TextMeshProUGUI healthCurValue;
+    public TextMeshProUGUI healthMaxValue;
+
+    public TextMeshProUGUI waterCurValue;
+    public TextMeshProUGUI waterMaxValue;
+
+    public TextMeshProUGUI rankCurValue;
+    public TextMeshProUGUI rankMaxValue;
+
+    public TextMeshProUGUI dungCurValue;
+    public TextMeshProUGUI dungMaxValue;
+
+    public TextMeshProUGUI diseaseCurValue;
+    public TextMeshProUGUI diseaseMaxValue;
+
+    public void OpenSettings()
+    {
+        // TODO
+    }
+
+    public void OpenStatistics()
+    {
+        this.overlay.SetActive(true);
+    }
+
+    public void CloseStatistics()
+    {
+        this.overlay.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.overlay.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
