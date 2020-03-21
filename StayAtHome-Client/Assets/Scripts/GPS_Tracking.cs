199174f5763693501ca6dff628ca5937f10278bd
karboe <karboe93@gmail.com>
2020-03-21 14:54:43 +0100

    makes longlat readable

--- StayAtHome-Client/Assets/Scripts/GPS_Tracking.cs
+++ StayAtHome-Client/Assets/Scripts/GPS_Tracking.cs
@@ -12,62 +12,109 @@ public class GPS_Tracking : MonoBehaviour
 {
 
     [SerializeField]
-    TextMeshProUGUI info;
+    TextMeshProUGUI info; //Remove
 
-    IEnumerator Start()
+    float latitude, longitude;
+
+    private void Start()
     {
-#if PLATFORM_ANDROID
+   
+        StartCoroutine(CheckForGPSAccess());
+        
+    }
+
+
+    IEnumerator CheckForGPSAccess()
+    {
+    #if PLATFORM_ANDROID
         if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
-            info.SetText("GPS is needed. Please allow Location access");
+            Debug.Log("GPS is needed. Please allow Location access");
             yield return 0;
             Permission.RequestUserPermission(Permission.FineLocation);
+            while (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                Debug.Log("Needs permission");
+                yield return new WaitForSeconds(2);
+            }
+            StartCoroutine(InitializeGPS());
 
-           
         }
         else
         {
-             if(!Input.location.isEnabledByUser){
-          info.SetText("GPS is required");
-          yield break;
-      }
+            StartCoroutine(InitializeGPS());
+        }
 
-            // Start service before querying location
-            Input.location.Start();
 
-            // Wait until service initializes
-            int maxWait = 20;
-            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-            {
-                info.SetText(maxWait.ToString());
-                maxWait--;
-            }
 
-            // Service didn't initialize in 20 seconds
-            /*if (maxWait < 1)
-            {
-                info.SetText("Timed out");
-                yield break;
-            }*/
+    #endif
 
-            // Connection has failed
-            if (Input.location.status == LocationServiceStatus.Failed)
-            {
-                info.SetText("Unable to determine device location");
-                yield break;
-            }
-            else
-            {
-                // Access granted and location value could be retrieved
-                info.SetText("Latitude: " + Input.location.lastData.latitude + " Longitude" + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-            }
+    }
 
+
+
+    IEnumerator InitializeGPS()
+    {
+        // Start service before querying location
+        Input.location.Start();
+
+
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("GPS is required");
+            yield break;
+        }
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            SetLatAndLong();
+            Debug.Log("Latitude: " + GetLatitude().ToString() + "\n Longitude: " + GetLongitude().ToString());
+            info.SetText("Latitude: " + GetLatitude().ToString() + "\n Longitude: " + GetLongitude().ToString());//Remove
+           
             // Stop service if there is no need to query location updates continuously
             Input.location.Stop();
         }
 
-        // First, check if user has location service enabled
-#endif
       
+    }
+    void SetLatAndLong()
+    {
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+    }
+
+    float GetLatitude()
+    {
+        return latitude;
+    }
+
+    float GetLongitude()
+    {
+        return longitude;
     }
 }
