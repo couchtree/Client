24daef982a67e745233bad9e38e47150ce7e623b
prooxeydev <louis1411@gmail.com>
2020-03-22 10:30:03 +0100

    Updated update script

--- update.bat
+++ update.bat
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd %HOME%
+echo "Pull Client..."
 git pull
 git submodule update --init --recursive
+echo "Pull Illustration..."
+cd StayAtHome-Client\Assets\Sprites\Illustration
+git pull
