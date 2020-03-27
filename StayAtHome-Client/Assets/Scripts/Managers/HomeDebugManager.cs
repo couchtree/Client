2c031f2fd7c922da68c0c3b5d8a18a78c2d6ff1c
CaluvH <opti01@gmx.net>
2020-03-27 11:14:42 +0100

    [197] Adding debug view on home screen.
    Showing homeBase, current location and current score.

---
+++ StayAtHome-Client/Assets/Scripts/Managers/HomeDebugManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Core.Map;
+
+namespace Managers
+{
+    public class HomeDebugManager : MonoBehaviour
+    {
+        public bool enableDebugging = false;
+        public TextMeshProUGUI homeLat;
+        public TextMeshProUGUI homeLon;
+        public TextMeshProUGUI currentLat;
+        public TextMeshProUGUI currentLon;
+        public TextMeshProUGUI score;
+
+        public Player player;
+        public GPS_Tracking gps;
+
+        private void Start()
+        {
+            if (!enableDebugging)
+            {
+                var thisGameObject = GetComponent<GameObject>();
+                thisGameObject.SetActive(false);
+            }
+        }
+
+        void Update()
+        {
+            if (enableDebugging)
+            {
+                homeLat.text = player.Lat.ToString();
+                homeLon.text = player.Lon.ToString();
+                currentLat.text = gps.GetLatitude().ToString();
+                currentLon.text = gps.GetLongitude().ToString();
+                score.text = player.Score.ToString();
+            }
+        }
+    }
+}
