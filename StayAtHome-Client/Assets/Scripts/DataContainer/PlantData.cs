2a442b3f804c6ce2e8f2f66ccfb5762ed5718bd0
CaluvH <opti01@gmx.net>
2020-03-27 22:06:46 +0100

    [197] Converting class to struct.

--- StayAtHome-Client/Assets/Scripts/DataContainer/PlantData.cs
+++ StayAtHome-Client/Assets/Scripts/DataContainer/PlantData.cs
@@ -4,7 +4,7 @@
     /// Struct to store all plant related data.
     /// It acts as a container for save and load operations w/o the need to instantiate the whole derived APlant-class
     /// </summary>
-    public class PlantData
+    public struct PlantData
     {
         public string name; // name of the plant
         public int evolutionLevel; // evoluation level of the plant
